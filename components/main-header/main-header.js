a09c39f85846365489494938248c5e1805eba5ee
Risal Walangadi <risal.walangadi@gmail.com>
2024-06-03 21:43:52 +0800

    add NavLink component

--- components/main-header/main-header.js
+++ components/main-header/main-header.js
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import Image from "next/image";
 
+import NavLink from "./nav-link";
 import logoImg from "@/assets/logo.png";
 import styles from "./main-header.module.css";
 
@@ -15,10 +16,10 @@ export default function MainHeader() {
       <nav className={styles.nav}>
         <ul>
           <li>
-            <Link href={"/meals"}>Browse Meals</Link>
+            <NavLink href={"/meals"}>Browse Meals</NavLink>
           </li>
           <li>
-            <Link href={"/community"}>Foodies Community</Link>
+            <NavLink href={"/community"}>Foodies Community</NavLink>
           </li>
         </ul>
       </nav>
