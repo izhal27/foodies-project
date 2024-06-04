25d69479ffe2d78b746ec11d7a9b66677d8b1d5a
Risal Walangadi <risal.walangadi@gmail.com>
2024-06-05 05:54:10 +0800

    fix meal detail page

--- app/meals/[slug]/page.js
+++ app/meals/[slug]/page.js
@@ -1,9 +1,32 @@
-export default function MealDetails({ params }) {
+import Image from "next/image";
+
+import styles from "./page.module.css";
+import { getMeal } from "@/lib/meals";
+
+export default async function MealDetails({ params }) {
+  const meal = await getMeal(params.slug);
+  meal.instructions = meal.instructions.replace(/\n/g, "<br />");
+
   return (
-    <main>
-      <h1 style={{ color: "white", textAlign: "center" }}>
-        Meal details [{params.slug}]
-      </h1>
-    </main>
+    <>
+      <header className={styles.header}>
+        <div className={styles.image}>
+          <Image src={meal.image} alt={meal.title} fill />
+        </div>
+        <div className={styles.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={styles.creator}>
+            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={styles.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main>
+        <p
+          className={styles.instructions}
+          dangerouslySetInnerHTML={{ __html: meal.instructions }}
+        ></p>
+      </main>
+    </>
   );
 }
