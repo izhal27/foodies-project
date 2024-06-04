bc81be34b675ac32ed80045cb1f3c30f9bbf19ee
Risal Walangadi <risal.walangadi@gmail.com>
2024-06-04 21:14:13 +0800

    refactor loading meals

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,11 +1,16 @@
+import { Suspense } from "react";
 import Link from "next/link";
-import styles from "./page.module.css";
+
 import MealsGrid from "@/components/meals/meals-grid";
 import { getMeals } from "@/lib/meals";
+import styles from "./page.module.css";
+
+async function Meals() {
+  const meals = await getMeals();
+  return <MealsGrid meals={meals} />;
+}
 
 export default function MealsPage() {
-  const meals = getMeals();
-
   return (
     <>
       <header className={styles.header}>
@@ -21,7 +26,11 @@ export default function MealsPage() {
         </p>
       </header>
       <main className={styles.main}>
-        <MealsGrid meals={meals} />
+        <Suspense
+          fallback={<p className={styles.loading}>Fetching meals...</p>}
+        >
+          <Meals />
+        </Suspense>
       </main>
     </>
   );
